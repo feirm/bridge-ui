657dd19a01a3a92871542f70ffd48c45bc7785e9
Jack Coble <jack@coble.uk>
2021-03-24 21:09:22 +0000

    Set Ethereum and Web3 window properties

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,14 @@ import router from './router'
 // Tailwind CSS
 import "@/assets/css/index.css";
 
+declare global {
+    interface Window {
+        ethereum: any;
+        web3: any;
+    }
+}
+
+window.ethereum = window.ethereum || {};
+window.web3 = window.web3 || {};
+
 createApp(App).use(router).mount('#app')
