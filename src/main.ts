c5bc16e7eb3f964396f945420f732bf95a77e40a
Jack Coble <jack@coble.uk>
2021-03-24 20:40:56 +0000

    Tailwind CSS

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Tailwind CSS
+import "@/assets/css/index.css";
+
 createApp(App).use(router).mount('#app')
