18fa8923e5409a3e3708e163c4e598bdeb56d8c1
Jack Coble <jack@coble.uk>
2021-04-23 16:28:28 +0100

    Address and signature inputs

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -6,70 +6,87 @@
       <img class="mx-auto w-16 mb-5" src="@/assets/images/logo.png" alt="Feirm Logo"/>
 
       <!-- Step 0: Connect to Metamask -->
-      <div v-if="formStep === 0">
-        <h1 class="text-3xl font-light mb-4 text-center">Feirm Blockchain Bridge 🌉</h1>
+      <div v-if="formStep === 0" class="space-y-4">
+        <h1 class="text-3xl font-light text-center">Feirm Blockchain Bridge 🌉</h1>
 
-        <p class="font-light mb-4">
+        <p class="font-light">
           Welcome to the Feirm Blockchain Bridge. This web application will allow you to migrate your native blockchain XFE coins into the brand-new XFE token recently launched on the Binance Smart Chain.
         </p>
 
-        <p class="font-light mb-3" v-if="!advanced">To ensure a smooth migration, please make sure you have the Metamask extension installed and configured for your browser.</p>
+        <p class="font-light mb-3">To get started, please enter your Binance Smart Chain (BSC) address in the field below.</p>
+        <label class="block">BSC Address</label>
+          <input
+            class="w-full border-2 border-gray-200 p-2 rounded outline-none focus:border-orange-500 transition duration-200"
+            type="text"
+            v-model="address"
+          />
 
-        <p class="font-light text-center mb-3" v-if="advanced"><b>⚠️ Warning! This is an advanced option! ⚠️</b></p>
-        <p class="font-light mb-3" v-if="advanced">Please enter your custom Binance Smart Chain address below. Feirm does not officially endorse this option, so use it at your own risk! Your XFE tokens could be at stake.</p>
-
-        <!-- Connect to Metamask button -->
-        <button v-if="!advanced"
-          class="block mb-2 w-full bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300"
-          @click="loadWeb3()">Connect to Metamask
-        </button>
-
-        <!-- Advanced settings -->
-        <button class="block rounded w-full p-4 bg-gray-200 text-gray-900" @click="advanced = !advanced" v-if="!advanced">Advanced Options</button>
-
-        <!-- Custom address -->
-        <input v-if="advanced" class="w-full border-2 mb-3 border-gray-200 p-3 rounded outline-none focus:border-orange-500 transition duration-200" v-model="address" type="text" placeholder="Please enter your Binance Smart Chain address" />
-        <button v-if="advanced"
-          @click="submitCustomAddress"
-          class="block mb-2 w-full bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300"
-          >Submit
-        </button>
-
-        <button class="block rounded w-full p-4 bg-red-400 text-red-900" v-if="advanced" @click="goBack">Go back!</button>
-      </div>
-
-      <!-- Step 1: Swap request details -->
-      <div v-if="formStep === 1">
-        <h1 class="text-3xl font-light mb-4 text-center">Swap Request Details</h1>
-        <p class="font-light mb-2">
-          Please send your current XFE coins to the deposit address below. Once the transaction has been confirmed, the new XFE token on the Binance Smart Chain will automatically be sent to your Metamask Wallet.
-        </p>
-        <hr>
-
-        <!-- Request details -->
-        <div class="mt-5 mb-3 text-center">
-          <!-- Address QR -->
-          <img class="mx-auto mb-3" :src="addressQr" alt="Deposit Address QR">
-
-          <!-- Address -->
-          <code>
-            {{ request.deposit_address }}
-          </code>
-        </div>
-
-        <!-- Request new swap -->
-        <hr>
-        <p class="font-light mt-2 mb-3">
-          <b>Please do not use the same address for more than one swap!</b>
-          If you wish to carry out another swap, please generate a new swap request using the button below.</p>
         <button
           class="block w-full bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300"
-          @click="newSwapRequest()">Get a new swap request
+          @click="goForward">Next
         </button>
       </div>
 
-      <!-- Step 2: Bridge disabled -->
-      <div v-if="formStep === 2">
+      <!-- Step 1: Swap request preparation -->
+      <div v-if="formStep === 1" class="space-y-3">
+        <h1 class="text-3xl font-light mb-4 text-center">Preparation</h1>
+        <p class="font-light">In order to claim your Feirm tokens, you need your existing wallet to hand. You will use this to produce cryptographic signatures which verify that you own an address.</p>
+        <p class="font-light">Before going further, we suggest that you load up the Feirm QT Desktop wallet and navigate to:</p>
+        <p>
+          <b>File > Sign message</b>
+        </p>
+        <p class="font-light">Once you are ready, please click the button below to move forward.</p>
+        <button class="block rounded w-full p-4 bg-orange-500 text-yellow-900" @click="goForward">Next</button>
+      </div>
+
+      <!-- Step 2: Swap request details -->
+      <div v-if="formStep === 2" class="space-y-3">
+        <h1 class="text-3xl font-light mb-4 text-center">Address signature</h1>
+        <p class="font-light">For your native Feirm (XFE) address, please produce a signature for the following message. Make sure you copy it exactly otherwise signature verification will fail.</p>
+
+        <div class="border-2 border-gray-200 rounded p-2 text-center mx-auto text-xs">
+          <code>Binance:{{ address }}</code>
+        </div>
+
+        <p class="font-light">Please enter your address and produced signature below. After you have filled it in, click the submit button to process your request.</p>
+        <p class="font-light">If you did everything correctly, you will receive your tokens on the Binance Smart Chain instantly.</p>
+
+        <div class="p-3 bg-white rounded space-y-3">
+          <label class="block">Native Feirm (XFE) Address</label>
+          <input
+            class="w-full border-2 border-gray-200 p-2 rounded outline-none focus:border-orange-500 transition duration-200"
+            type="text"
+            v-model="swap.address"
+          />
+
+          <label class="block">Signature</label>
+          <input
+            class="w-full border-2 border-gray-200 p-2 rounded outline-none focus:border-orange-500 transition duration-200"
+            type="text"
+            v-model="swap.signature"
+          />
+        </div>
+
+        <button :disabled="submitted" class="block disabled:opacity-50 rounded w-full p-4 bg-orange-500 text-yellow-900" @click="sendRequest">Submit</button>
+      </div>
+
+      <!-- Step 3: Confirmation -->
+      <div v-if="formStep === 3" class="space-y-3">
+        <h1 class="text-3xl font-light mb-4 text-center">Success!</h1>
+        <p class="font-light">Thanks for using the Feirm Bridge. You should have received your tokens on the Binance Smart Chain. More details about your request can be found below.</p>
+
+        <div class="p-3 bg-white rounded space-y-3">
+          <p>Transaction ID (Binance Smart Chain):</p>
+          <a class="text-sm" :href="`https://bscscan.com/tx/${response.sent_hash}`" target="_blank">
+            <b>{{ response.sent_hash.substring(0, 46) + '...' }}</b>
+          </a>
+        </div>
+
+        <button @click="goBack" class="block rounded w-full p-4 bg-orange-500 text-yellow-900">New request</button>
+      </div>
+
+      <!-- Step 10: Bridge disabled -->
+      <div v-if="formStep === 10">
         <h1 class="text-3xl font-light mb-4 text-center">Feirm Blockchain Bridge 🌉</h1>
         <p class="font-light mb-2">
           Sorry, the blockchain bridge is in maintenance mode! Please check back later.
@@ -83,88 +100,67 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import web3 from "web3";
-import qrcode from "qrcode";
 import bridgeService from "@/service/bridgeService";
+import { Addresses, SwapRequest } from '@/models/swap';
 
 export default defineComponent({
   name: 'Index',
   data() {
     return {
       address: "",
-      addressQr: "",
-      request: "" as any,
-      advanced: false,
-      formStep: 2
+
+      swap: {
+        address: "",
+        signature: ""
+      },
+
+      response: "",
+      submitted: false,
+      formStep: 0
     }
   },
   methods: {
-    async loadWeb3() {
-      if (window.ethereum) {
-        window.web3 = new web3(window.ethereum);
-        await window.ethereum.enable();
+    goForward() {
+      if (!this.address) {
+        return this.$toast.error("BSC Address cannot be empty!");
       }
 
-      // Get address from Metamask
-      const accounts = await window.web3.eth.getAccounts();
-      this.address = accounts[0];
-
-      // Create a swap request
-      // Use the first address in the accounts array
-      try {
-        const res = await bridgeService.CreateRequest(this.address);
-        this.request = res.data;
-
-        // Generate QR code of deposit address
-        // To be compatible with Feirm PWA, append Feirm infront
-        this.addressQr = await qrcode.toDataURL("feirm:" + this.request.deposit_address)
-      } catch (e) {
-        return this.$toast.error(e.response.data.error);
-      }
-      
-      // Proceed to next step
-      this.formStep++;
-    },
-    async submitCustomAddress() {
-      // Create a swap request
-      try {
-        const res = await bridgeService.CreateRequest(this.address);
-        this.request = res.data;
-
-        // Generate QR code of deposit address
-        // To be compatible with Feirm PWA, append Feirm infront
-        this.addressQr = await qrcode.toDataURL("feirm:" + this.request.deposit_address)
-      } catch (e) {
-        return this.$toast.error(e.response.data.error);
-      }
-      
-      // Proceed to next step
       this.formStep++;
     },
     goBack() {
-      // Clear address
-      this.address = "";
-
-      // Toggle advanced state
-      this.advanced = false;
+      // Clear swap
+      this.swap.address = "";
+      this.swap.signature = "";
+      this.formStep--;
     },
-    async newSwapRequest() {
-      // Create a swap request
-      // Use the first address in the accounts array
+    async sendRequest() {
+      this.submitted = true;
+
+      // Construct the address payload
+      const addressPayload = {
+        address: this.swap.address,
+        signature: this.swap.signature
+      } as Addresses;
+
+      // New request object
+      const request = {
+        receiving_address: this.address,
+        addresses: [
+          addressPayload
+        ]
+      } as SwapRequest;
+
+      // Submit to API
       try {
-        const res = await bridgeService.CreateRequest(this.address);
-        this.request = res.data;
-
-        // Generate QR code of deposit address
-        // To be compatible with Feirm PWA, append Feirm infront
-        this.addressQr = await qrcode.toDataURL("feirm:" + this.request.deposit_address)
+        const res = await bridgeService.CreateRequestV2(request);
+        this.response = res.data;
       } catch (e) {
-        if (e.response.status == 429) {
-          return this.$toast.error(e.response.data);
-        }
-
-        return this.$toast.error(e.response.data.error)
+        this.submitted = false;
+        return this.$toast.error(e.response.data.error);
       }
+
+      this.submitted = false;
+      this.formStep++;
     }
   }
 });
